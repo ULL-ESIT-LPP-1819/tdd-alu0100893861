ffc476d87766c3d0798aca7d2b386e0cb0897a5e
Jaime Rodriguez <alu0100893861@ull.edu.es>
2018-11-07 18:21:53 +0000

    Existe la cantidad de azucares

--- lib/gema/etiqueta_nutricional.rb
+++ lib/gema/etiqueta_nutricional.rb
@@ -1,10 +1,11 @@
 class Etiqueta
-	attr_reader :nombre, :grasa,:saturadas, :hidratos, :proteinas
-	def initialize(nombre,grasa,saturadas, hidratos, proteinas)
+	attr_reader :nombre, :grasa,:saturadas, :hidratos, :proteinas, :azucares
+	def initialize(nombre,grasa,saturadas, hidratos, proteinas,azucares)
 		@nombre=nombre
 		@grasa=grasa
 		@saturadas=saturadas
 		@hidratos=hidratos
 		@proteinas=proteinas
+		@azucares=azucares
 	end
 end
