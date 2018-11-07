83723e2e790e86aae6452881f173c079dcd34a2b
Jaime Rodriguez <alu0100893861@ull.edu.es>
2018-11-07 19:10:10 +0000

    No existe un metodo para calcular el valor energetico

--- lib/gema/etiqueta_nutricional.rb
+++ lib/gema/etiqueta_nutricional.rb
@@ -1,5 +1,5 @@
 class Etiqueta
-	attr_reader :nombre, :grasa,:saturadas, :hidratos, :proteinas, :azucares, :sal
+	attr_reader :nombre, :grasa,:saturadas, :hidratos, :proteinas, :azucares, :sal, :valor_energetico
 	def initialize(nombre,grasa,saturadas, hidratos, proteinas,azucares, sal)
 		@nombre=nombre
 		@grasa=grasa
@@ -8,5 +8,12 @@ class Etiqueta
 		@proteinas=proteinas
 		@azucares=azucares
 		@sal=sal
+		@valor_energetico=[v_energeticokJ,v_energeticokc]
+	end
+	def v_energeticokJ
+		(37*(grasa+saturadas))+(17*(hidratos+azucares))+(17*proteinas)
+	end
+	def v_energeticokc
+		(9*(grasa+saturadas))+(9*(hidratos+azucares))+(17*proteinas)
 	end
 end
--- spec/gema_spec.rb
+++ spec/gema_spec.rb
@@ -4,32 +4,33 @@ RSpec.describe Gema do
   end
 
 	describe Etiqueta do
-	   	p = Etiqueta.new("galleta",20,2,30,50,25,40)
+	   	p = Etiqueta.new("galleta",70,20,260,50,90,6)
 		context "Caracteristicas de alimentos" do 
 			it "Debe existir un nombre para la etiqueta" do
 		  		 expect("galleta").to eq(p.nombre)
 			end
 			it "Debe existir la cantidad de grasa" do 
-				expect(20).to eq(p.grasa)
+				expect(70).to eq(p.grasa)
 			end
 			it "Debe existir la cantidad de hidratos de carbono" do
-				expect(30).to eq(p.hidratos)
+				expect(20).to eq(p.hidratos)
 			end
 			it "Debe existir la cantidad de grasas saturadas" do
-				expect(2).to eq(p.saturadas)
+				expect(260).to eq(p.saturadas)
 			end
 			it "Debe existir la cantidad de proteinas" do
 				expect(50).to eq(p.proteinas)
 			end
 			it "Debe existir la cantidad de azucares" do
-				expect(25).to eq(p.azucares)
+				expect(90).to eq(p.azucares)
 			end
 			it "Debe existir la cantidad de sal" do
-				expect(40).to eq(p.sal)
+				expect(6).to eq(p.sal)
 			end
 		context "Existencia de Métodos" do
 			it "Debe existir un metodo para calcular el valor nutricional" do
-				expect(40).to eq(p.valor_nutricional)
+				expect(2000).to eq(p.valor_energetico[0])
+				expect(2000).to eq(p.valor_energetico[1])
 			end	
 		end
 		end
