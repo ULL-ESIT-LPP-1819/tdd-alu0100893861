0983e3f50c5e9946e7cbe1ca7e6e9e2c034d1462
Jaime Rodriguez <alu0100893861@ull.edu.es>
2018-11-08 14:12:36 +0000

    El metodo para calcular el valor energetico ya existe

--- lib/gema/etiqueta_nutricional.rb
+++ lib/gema/etiqueta_nutricional.rb
@@ -8,12 +8,13 @@ class Etiqueta
 		@proteinas=proteinas
 		@azucares=azucares
 		@sal=sal
+		@fibra_alimentaria=2.1
 		@valor_energetico=[v_energeticokJ,v_energeticokc]
 	end
 	def v_energeticokJ
-		(37*(grasa+saturadas))+(17*(hidratos+azucares))+(17*proteinas)
+		(37.0*@grasa)+(17.0*@hidratos)+(17.0*@proteinas)+(25*@sal)
 	end
 	def v_energeticokc
-		(9*(grasa+saturadas))+(9*(hidratos+azucares))+(17*proteinas)
+		(9.0*@grasa)+(4.0*@hidratos)+(4.0*@proteinas)+(6*@sal)
 	end
 end
--- spec/gema_spec.rb
+++ spec/gema_spec.rb
@@ -4,33 +4,33 @@ RSpec.describe Gema do
   end
 
 	describe Etiqueta do
-	   	p = Etiqueta.new("galleta",70,20,260,50,90,6)
+		p = Etiqueta.new("galleta",10.5,5.10,77,7.6,24,0.83)
 		context "Caracteristicas de alimentos" do 
 			it "Debe existir un nombre para la etiqueta" do
 		  		 expect("galleta").to eq(p.nombre)
 			end
 			it "Debe existir la cantidad de grasa" do 
-				expect(70).to eq(p.grasa)
+				expect(10.5).to eq(p.grasa)
 			end
 			it "Debe existir la cantidad de hidratos de carbono" do
-				expect(20).to eq(p.hidratos)
+				expect(77).to eq(p.hidratos)
 			end
 			it "Debe existir la cantidad de grasas saturadas" do
-				expect(260).to eq(p.saturadas)
+				expect(5.10).to eq(p.saturadas)
 			end
 			it "Debe existir la cantidad de proteinas" do
-				expect(50).to eq(p.proteinas)
+				expect(7.6).to eq(p.proteinas)
 			end
 			it "Debe existir la cantidad de azucares" do
-				expect(90).to eq(p.azucares)
+				expect(24).to eq(p.azucares)
 			end
 			it "Debe existir la cantidad de sal" do
-				expect(6).to eq(p.sal)
+				expect(0.83).to eq(p.sal)
 			end
 		context "Existencia de Métodos" do
 			it "Debe existir un metodo para calcular el valor nutricional" do
-				expect(2000).to eq(p.valor_energetico[0])
-				expect(2000).to eq(p.valor_energetico[1])
+				expect(1847.45).to eq(p.valor_energetico[0])
+				expect(437.88).to eq(p.valor_energetico[1])
 			end	
 		end
 		end
